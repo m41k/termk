0649bdbb2e88b117a7635131547c737aa33e1e2a
m41k <maik.alberto@hotmail.com>
2018-08-27 17:53:04 -0300

    Update termk.sh

--- termk.sh
+++ termk.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-
+clear
 SERVER="https://raw.githubusercontent.com/m41k/termk/master/server"
-
 case $1 in
  create)
-  wget -qO- $(wget -qO- $SERVER)
+   ID=$(wget -qO- $(wget -qO- $SERVER) | sed '/^$/d')
+   wget -qO- $(wget -qO- $SERVER)/?$ID | sed '/^$/d'
+   echo -n "Enter captcha: "
+   read DIG
+   clear
+   wget -qO- $(wget -qO- $SERVER)/?$ID:$DIG | sed '/^$/d'
  ;;
 esac
