dc5e1c0f4d3bb64e81986ec9ec5c2ad9a1efe92f
m41k <maik.alberto@hotmail.com>
2018-09-01 19:14:26 -0300

    Update termk.sh

--- termk.sh
+++ termk.sh
@@ -9,12 +9,15 @@
 clear
 SERVER="https://raw.githubusercontent.com/m41k/termk/master/server"
 case $1 in
- create | *)
+ list)
+   wget -qO- $(wget -qO- $SERVER)/?$1 | sed '/^$/d'
+ ;;
+ create)
    ID=$(wget -qO- $(wget -qO- $SERVER) | sed '/^$/d')
    wget -qO- $(wget -qO- $SERVER)/?$ID | sed '/^$/d'
    echo -n "Repeat the characters: "
    read DIG
    clear
-   wget -qO- $(wget -qO- $SERVER)/?$ID:$DIG | sed '/^$/d'
+   wget -qO- $(wget -qO- $SERVER)/?$ID:$DIG@$2 | sed '/^$/d'
  ;;
 esac
